1892c4543c3569e829f9577dda2c68936eeae1b6
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-07-06 17:07:57 +0530

    toast functionality added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,11 @@
 const modalOverlay = document.querySelector(".modal-overlay");
 const modalCloseBtn = document.querySelector(".modal-close-btn");
+const toastCloseBtn = document.querySelector(".toast-close-btn");
+const toast = document.querySelector(".notification-toast-container");
 
 modalCloseBtn.addEventListener("click", () => {
   modalOverlay.classList.remove("show-modal");
+});
+toastCloseBtn.addEventListener("click", () => {
+  toast.classList.add("stop-animation");
 });
